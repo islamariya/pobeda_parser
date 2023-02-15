8385b592f86b894968732d52ad0b024035a25eb0
maria <maria.s.sergeeva@gmail.com>
2023-02-15 13:29:13 +0500

    same orign and destination point deleted, del weekdays from search

--- parser.py
+++ parser.py
@@ -16,7 +16,7 @@ def generate_date_range() -> list[datetime]:
     search_end_date = datetime.date(day=20, month=10, year=2023)
     day_difference = (search_end_date - search_start_date).days
     date_range = [dd for dd in [search_start_date + datetime.timedelta(days=i) for i in range(day_difference)]
-                  if dd.month in (3, 4, 9, 10) and dd.weekday() in (1, 2, 3, 4, 5)]
+                  if dd.month in (3, 4, 9, 10) and dd.weekday() in (0, 1, 2, 3, 4)]
     return date_range
 
 
@@ -28,29 +28,40 @@ def get_price(tag_data) -> int:
 
 
 def check_price(dd, origin_code, destination_code):
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    if origin_code == destination_code:
+        return
+
+    prices_found = []
     formatted_date = dd.strftime(pobeda_dt_format)
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     url = f"https://ticket.pobeda.aero/websky/?origin-city-code%5B0%5D={origin_code}" \
           f"&destination-city-code%5B0%5D={destination_code}&date%5B0%5D={formatted_date}" \
           f"&segmentsCount=1&adultsCount=1&youngAdultsCount=0&childrenCount=0&" \
           f"infantsWithSeatCount=0&infantsWithoutSeatCount=0&lang=ru#/search"
+
     driver.get(url)
-    time.sleep(3)
+    time.sleep(2)
+
     html_source = driver.page_source
+
     soup = BeautifulSoup(html_source, features="html.parser")
     tag_data = soup.find_all('span', attrs={'class': 'price-cell__text'})
-    prices_found = []
+
     for item in tag_data:
         price = get_price(tag_data=item)
         prices_found.append(price)
+
     min_price = min(prices_found) if prices_found else 0
+
     message = f"Вот мин цена {min_price} {origin_code} {destination_code} {formatted_date}"
     logger.info(message)
-    #print("вот min цена", min_price, origin_code, destination_code, formatted_date)
+
     if min_price in price_range:
         logger_message = f"{origin_code}-{destination_code} {formatted_date} {min_price}"
-        logger.info(logger_message)
-        logger.info(url)
+        logger.warning(logger_message)
+        logger.warning(url)
+
     driver.close()
 
 
